2667cfb34c35bea3e7830df3ab9720de5f08e617
Miguel Nieto Lopez <58054182+MiguelNietoLopez@users.noreply.github.com>
2023-02-26 18:17:37 -0600

    Update CheckEachLengthOfCSVString.cs

--- C#/CheckEachLengthOfCSVString.cs
+++ C#/CheckEachLengthOfCSVString.cs
@@ -1,3 +1,5 @@
+//Takes CSV in string form, seperates them into an array, checks the length of each item, and writes to console order number
+//if its equal to 4 otherwise writes order number + '   - Error'
 string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
 string[] orderArray = orderStream.Split(',');
 Array.Sort(orderArray);
